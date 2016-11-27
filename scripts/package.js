2f0c044fec625d4e07fa963956388c026165fe7e
Aaron Ballard <ans.ballard@gmail.com>
2016-11-27 11:11:23 -0600

    initial installer logic, **WIP**

--- scripts/package.js
+++ scripts/package.js
@@ -1,28 +1,59 @@
 #!/usr/bin/env node
 
 const packager = require("electron-packager");
+const installer = require("electron-installer-windows");
 const pkg = require("../package.json");
 
-packager({
-  arch: "ia32",
-  dir: "./",
-  ignore: "node_modules|src|packaged|scripts",
-  platform: "win32",
-  asar: true,
-  name: "Modwatch",
-  out: "./packaged",
-  overwrite: true,
-  prune: true,
-  version: pkg.devDependencies["electron-prebuilt"],
-  "version-string": {
-    CompanyName: "Modwatch",
-    FileDescription: "A simple uploader for modwat.ch",
-    OriginalFilename: "Modwatch.exe",
-    ProductName: "Modwatch",
-    InternalName: "Modwatch"
-  }
-}, (err, paths) => {
-  if(err) {
-    console.log(err);
-  }
+const opts = {
+  packagedDirectory: "./packaged",
+  fullPackagedDirectory: `${packagedDirectory}/Modwatch-win32-x86`,
+  installerDirectory: "./installer"
+};
+
+new Promise((resolve, reject) => {
+  packager({
+    arch: "ia32",
+    dir: "./",
+    ignore: "node_modules|src|packaged|scripts",
+    platform: "win32",
+    asar: true,
+    name: "Modwatch",
+    out: "./packaged",
+    overwrite: true,
+    prune: true,
+    version: pkg.devDependencies["electron-prebuilt"],
+    "version-string": {
+      CompanyName: "Modwatch",
+      FileDescription: "A simple uploader for modwat.ch",
+      OriginalFilename: "Modwatch.exe",
+      ProductName: "Modwatch",
+      InternalName: "Modwatch"
+    }
+  }, (err, paths) => {
+    if(err) {
+      reject(err);
+    } else {
+      resolve(paths);
+    }
+  });
+})
+.then(paths => new Promise((resolve, reject) => {
+  installer({
+    src: opts.fullPackagedDirectory,
+    dest: opts.installerDirectory
+  }, err => {
+    if(err) {
+      reject(err);
+    } else {
+      resolve();
+    }
+  })
+}))
+.then(() => {
+  console.log("Installer Built!");
+  process.exit(1);
+})
+.catch(e => {
+  console.err("Package Generation Failed");
+  console.err(e);
 });
